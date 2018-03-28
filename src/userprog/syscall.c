e52d13dd454be8bdc7427b36d64da974b9d4b0aa
black8ow <thecrooks2@gmail.com>
2018-03-28 13:58:30 +0900

    syscall

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -31,7 +31,7 @@ void get_argument(void *esp, int *arg, int count) {
 
 }
 /* Check address of pointer point user domain */
-void check_address() {
+void check_address(void *addr) {
     if(!(0x80480000 < addr && addr < 0xc0000000))
         exit(-1);
 }
@@ -43,7 +43,8 @@ syscall_handler (struct intr_frame *f)
   thread_exit (); */
   int syscall_nr = *(int)f->esp;
   int arg[5];
-  
+  check_address(); // 주소값이 유효한지 확인
+
   switch(syscall_nr) {
       case SYS_HALT:
         halt();
@@ -66,3 +67,26 @@ syscall_handler (struct intr_frame *f)
   }
   
 }
+
+void halt(void){
+	shutdown_power_off(); //핀토스 종료
+}
+
+void exit(int status){
+	struct thread *current = thread_current(); //실행중인 스레드 구조체 정보
+	if(thread_alive(current->parent)
+		current->cp->status = status;
+	printf("%s: exit(%d)\n", current->name, status); // 스레드 이름과 exit status 출력
+	thread_exit(); // 스레드 종료
+}
+
+bool create(const char *file, unsigned initial_size){
+	bool success = filesys_create(file, initial_size); // 파일이름과 크기
+	return success; //파일 생성에 성공하면 true 리턴
+}
+
+bool remove(const char *file){
+	bool success = filesys_remove(file); // 파일 이름에 해당하는 파일 제거
+	return success; //파일 제거에 성공하면 true리턴 
+}
+
