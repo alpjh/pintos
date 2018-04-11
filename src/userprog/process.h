2c48bb3f78d9ebb93681a98e124fb8bba9b14ce5
black8ow <thecrooks2@gmail.com>
2018-04-12 01:31:57 +0900

    prototype

--- src/userprog/process.h
+++ src/userprog/process.h
@@ -8,4 +8,12 @@ int process_wait (tid_t);
 void process_exit (void);
 void process_activate (void);
 
-#endif /* userprog/process.h */
+int process_add_file(struct file *f);
+struct file *process_get_file (int fd);
+void process_close_file(int fd);
+struct thread *get_child_process (int pid); 
+void child_process (struct thread *cp);
+int count_token(const char* str);
+
+
+endif /* userprog/process.h */
