da984a0b3d7c3781eeebeaae224cb8770a8bf555
alpjh <alpjh@naver.com>
2018-04-12 12:44:02 +0900

    deny panic

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -20,163 +20,191 @@ bool remove (const char *file);
 
 void
 syscall_init (void) {
-  intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
+    intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
 //실습내용 sudo
 int open(const char *file){
-  lock_acquire(&filesys_lock);
-  struct fild *f = filesys_open(file);
+    lock_acquire(&filesys_lock);
+    struct fild *f = filesys_open(file);
 
-  int fd;
-  
-  //filesys_open fail
-  if (!f) {
+    int fd;
+
+    //filesys_open fail
+    if (!f) {
+        lock_release(&filesys_lock);
+        return -1;
+    }
+    fd = process_add_file(f);
+    //process_add_file 실패 경우도 생각해야함
     lock_release(&filesys_lock);
-    return -1;
-  }
-  fd = process_add_file(f);
-  //process_add_file 실패 경우도 생각해야함
-  lock_release(&filesys_lock);
-  return fd;
+    return fd;
 }
 
 int filesize(int fd){
-  lock_acquire(&filesys_lock);
-  struct file *f = process_get_file(fd);
-  if (!f){
+    lock_acquire(&filesys_lock);
+    struct file *f = process_get_file(fd);
+    if (!f){
+        lock_release(&filesys_lock);
+        return -1;
+    }
+    int size = file_length(f);
     lock_release(&filesys_lock);
-    return -1;
-  }
-  int size = file_length(f);
-  lock_release(&filesys_lock);
-  return size;
+    return size;
 }  //open이 되었다는 가정 하에 진행
 
 int read(int fd, void *buffer, unsigned size){
 
-  struct file *f = process_get_file(fd); 
-  
-  if (fd == 0){
-    unsigned i;
-    uint8_t* localbuf = (uint8_t*) buffer;
-    for (i = 0; i < size; i++){
-      localbuf[i] = input_getc();
-    }
-    return size;
-  } // fd값이 0인 파일은 파일크기가 없음. 키보드로부터 데이터를 읽어오는 >동작을 추가해줘야함
-  
-  lock_acquire(&filesys_lock);
-  if (!f) {
-    lock_release(&filesys_lock);
-    return -1;
-  }
+    struct file *f = process_get_file(fd); 
 
-  int bytesize = file_read(f, buffer, size);
-  
-  lock_release(&filesys_lock);
-  return bytesize;
+    if (fd == 0){
+        unsigned i;
+        uint8_t* localbuf = (uint8_t*) buffer;
+        for (i = 0; i < size; i++){
+            localbuf[i] = input_getc();
+        }
+        return size;
+    } // fd값이 0인 파일은 파일크기가 없음. 키보드로부터 데이터를 읽어오는 >동작을 추가해줘야함
+
+    lock_acquire(&filesys_lock);
+    if (!f) {
+        lock_release(&filesys_lock);
+        return -1;
+    }
+
+    int bytesize = file_read(f, buffer, size);
+
+    lock_release(&filesys_lock);
+    return bytesize;
 }
 
 int write(int fd, void *buffer, unsigned size){
 
-  if (fd == 1) {
-    putbuf(buffer,size); //파일디스크립터가 1일 경우 버퍼에 저장된 갑승ㄹ 화면에 출력하고 버퍼의 크기를 리턴
-    return size;
-  }
+    if (fd == 1) {
+        putbuf(buffer,size); //파일디스크립터가 1일 경우 버퍼에 저장된 갑승ㄹ 화면에 출력하고 버퍼의 크기를 리턴
+        return size;
+    }
 
-  lock_acquire(&filesys_lock);
-  struct file *f = process_get_file(fd);
+    lock_acquire(&filesys_lock);
+    struct file *f = process_get_file(fd);
 
-  if (!f){
+    if (!f){
+        lock_release(&filesys_lock);
+        return -1;
+    }
+
+    int bytesize = file_write(f, buffer, size);
+
     lock_release(&filesys_lock);
-    return -1;
-  }
-
-  int bytesize = file_write(f, buffer, size);
-
-  lock_release(&filesys_lock);
-  return bytesize;
+    return bytesize;
 }
 
 ////////////////////실습
 
 void seek (int fd, unsigned position){
-  lock_acquire(&filesys_lock);
-  struct file *f = process_get_file(fd);
-  
-  if(!f){
-    lock_release(&filesys_lock);
-    return;
-  }
+    lock_acquire(&filesys_lock);
+    struct file *f = process_get_file(fd);
 
-  file_seek(f, position); //열린 파일의 위치를 position만큼 이동
-  lock_release(&filesys_lock);
+    if(!f){
+        lock_release(&filesys_lock);
+        return;
+    }
+
+    file_seek(f, position); //열린 파일의 위치를 position만큼 이동
+    lock_release(&filesys_lock);
 }
 
 unsigned tell (int fd){
-  lock_acquire(&filesys_lock);
-  struct file *f = process_get_file(fd);
-  if(!f){
+    lock_acquire(&filesys_lock);
+    struct file *f = process_get_file(fd);
+    if(!f){
+        lock_release(&filesys_lock);
+        return -1;
+    }
+
+    off_t offset = file_tell(f);
     lock_release(&filesys_lock);
-    return -1;
-  }
- 
-  off_t offset = file_tell(f);
-  lock_release(&filesys_lock);
-  return offset; //열린 파일의 위치 리턴
+    return offset; //열린 파일의 위치 리턴
 }
 
 void close (int fd){
-  lock_acquire(&filesys_lock);
-  process_close_file(fd); //파일 닫음
-  lock_release(&filesys_lock);
+    process_close_file(fd); //파일 닫음
 }
 
 static void
 syscall_handler (struct intr_frame *f) {
 
-  uint32_t *esp = f->esp;// Get user stack pointer
-  check_address((void *)esp); // 주소값이 유효한지 확인
-  int syscall_nr = *esp; 
-  int arg[5];
+    uint32_t *esp = f->esp;// Get user stack pointer
+    check_address((void *)esp); // 주소값이 유효한지 확인
+    int syscall_nr = *esp; 
+    int arg[5];
 
-  printf("%d", thread_current);
-  printf("system call number : %d\n", syscall_nr);
-  /* System Call switch */
-  switch(syscall_nr) {
-      //HALT
-      case SYS_HALT:
-        halt();
-        break;
-      //EXIT
-      case SYS_EXIT :
-        get_argument(esp, arg, 1);
-        exit(arg[0]);
-        f->eax = (arg[0]);
-        break;
-      case SYS_EXEC :
-        get_argument(esp, arg, 1);
-        f->eax = exec((const char *)arg[0]);
-        break;
-      case SYS_WAIT :
-        get_argument(esp, arg, 1);
-        f->eax = wait(arg[0]);
-        break;
-      //CREATE
-      case SYS_CREATE :
-        get_argument(esp, arg, 2);
-        f->eax = create((const char *)arg[0], (const char *)arg[1]);
-        break;
-      //REMOVE
-      case SYS_REMOVE :
-        get_argument(esp, arg, 1);
-        f->eax = remove((const char *)arg[0]);
-        break;
-     /*default :
-        printf("Not system call! \n");
-        thread_exit();*/
-  }  
+    printf("%d", thread_current);
+    printf("system call number : %d\n", syscall_nr);
+    /* System Call switch */
+    switch(syscall_nr) {
+        //HALT
+        case SYS_HALT:
+            halt();
+            break;
+            //EXIT
+        case SYS_EXIT :
+            get_argument(esp, arg, 1);
+            exit(arg[0]);
+            f->eax = (arg[0]);
+            break;
+        case SYS_EXEC :
+            get_argument(esp, arg, 1);
+            f->eax = exec((const char *)arg[0]);
+            break;
+        case SYS_WAIT :
+            get_argument(esp, arg, 1);
+            f->eax = wait(arg[0]);
+            break;
+            //CREATE
+        case SYS_CREATE :
+            get_argument(esp, arg, 2);
+            f->eax = create((const char *)arg[0], (const char *)arg[1]);
+            break;
+            //REMOVE
+        case SYS_REMOVE :
+            get_argument(esp, arg, 1);
+            f->eax = remove((const char *)arg[0]);
+            break;
+        case SYS_OPEN :
+            get_argument(esp, arg, 1);
+            f->eax = open((const char *)arg[0]);
+            break;
+        case SYS_FILESIZE :
+            get_argument(esp, arg, 1);
+            f->eax = filesize(arg[0]);
+            break;
+        case SYS_READ :
+            get_argument(esp, arg , 3);
+            f -> eax = read(arg[0] , (void *)arg[1] , (unsigned)arg[2]);
+            break;
+        case SYS_WRITE :
+            get_argument(esp, arg , 3);
+            f -> eax = write(arg[0] , (const void *)arg[1] , (unsigned)arg[2]);
+            break;
+        case SYS_SEEK :
+            get_argument(esp , arg , 2);
+            seek(arg[0] , (unsigned) arg[1]);
+            break;
+
+        case SYS_TELL :
+            get_argument(esp , arg , 1);
+            f -> eax = tell(arg[0]);
+            break;
+
+        case SYS_CLOSE :
+            get_argument(esp , arg , 1);
+            close(arg[0]);  
+            break;
+            /*default :
+              printf("Not system call! \n");
+              thread_exit();*/
+    }  
 }
 
 /* Check if address point user domain */
@@ -187,7 +215,7 @@ void check_address(void *addr) {
 
 /* Copy Value in UserStack to Kernel */    
 void get_argument(void *esp, int *arg, int count) {
-    
+
     int i;
     int *ptr; //temp pointer
     esp += 4;
@@ -201,15 +229,15 @@ void get_argument(void *esp, int *arg, int count) {
 }
 
 void halt(void) {
-	shutdown_power_off(); //핀토스 종료
+    shutdown_power_off(); //핀토스 종료
 }
 
 void exit(int status) {
-	struct thread *current = thread_current(); //실행중인 스레드 구조체 정보
+    struct thread *current = thread_current(); //실행중인 스레드 구조체 정보
     /* 프로세스 디스크립터에 exit status 저장 */
     current -> exit_status = status;
-	printf("%s: exit(%d)\n", current->name, status); // 스레드 이름과 exit status 출력
-	thread_exit(); // 스레드 종료
+    printf("%s: exit(%d)\n", current->name, status); // 스레드 이름과 exit status 출력
+    thread_exit(); // 스레드 종료
 }
 
 tid_t exec(const char *cmd_line) {
@@ -221,7 +249,7 @@ tid_t exec(const char *cmd_line) {
 
     //wait for child process tapjae
     sema_down(&child->load_sema);
-    
+
     //if load success, return pid
     if (child->loaded) 
         return pid;
@@ -236,12 +264,12 @@ int wait(tid_t tid) {
 }
 
 bool create(const char *file, unsigned initial_size) {
-	bool success = filesys_create(file, initial_size); // 파일이름과 크기
-	return success; //파일 생성에 성공하면 true 리턴
+    bool success = filesys_create(file, initial_size); // 파일이름과 크기
+    return success; //파일 생성에 성공하면 true 리턴
 }
 
 bool remove(const char *file) {
-	bool success = filesys_remove(file); // 파일 이름에 해당하는 파일 제거
-	return success; //파일 제거에 성공하면 true리턴 
+    bool success = filesys_remove(file); // 파일 이름에 해당하는 파일 제거
+    return success; //파일 제거에 성공하면 true리턴 
 }
 
