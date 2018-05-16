d4439743c794f5983bb629c4c57846632c8af880
alpjh <alpjh@naver.com>
2018-05-16 16:17:09 +0900

    find ok

--- src/vm/page.c
+++ src/vm/page.c
@@ -24,9 +24,11 @@ static bool vm_less_func (const struct hash_elem *a,
     /* hash_entry()로 각각의 element에 대한 vm_entry 구조체 */
     struct vm_entry *vme_a = hash_entry (a, struct vm_entry, elem);
     struct vm_entry *vme_b = hash_entry (b, struct vm_entry, elem);
+    
     /*   vaddr비교 (b가크다면true, a가크다면false) */
     if (vme_a->vaddr < vme_b->vaddr)
         return true;
+    
     return false;
 }
 
@@ -41,10 +43,19 @@ bool delete_vme (struct hash *vm, struct vm_entry *vme) {
 }
 
 struct vm_entry *find_vme (void *vaddr) {
+    
+    struct vm_entry = vme;
+    struct hash_elem *e;
+
     /* pg_round_down()으로 vaddr의 페이지번호를 얻음 */
+    vme.vaddr = pg_round_down (vaddr);
     /* hash_find() 함수를 사용해서 hash_elem 구조체 얻음 */
+    if  (e = hash_find (&thread_current()->vm, &vme.elem)) {
+        /* hash_entry()로 해당 hash_elem의 vm_entry구조체 리턴 */
+        return hash_entry (e, struct vm_entry, elem);
+    }
     /* 만약 존재하지 않는다면 NULL리턴 */
-    /* hash_entry()로 해당 hash_elem의 vm_entry구조체 리턴 */
+    return NULL;
 }
 
 void vm_destroy (struct hash *vm) {
@@ -53,8 +64,10 @@ void vm_destroy (struct hash *vm) {
 }
 
 void vm_destroy_func (struct hash_elem *e, void *aux UNUSED) {
+    
     /* Get hash element (hash_entry() 사용) */
     struct vm_entry *vme = hash_entry (e, struct vm_entry, elem);
+    
     /* load가 되어있는 page의 vm_entry인 경우 */
     if (vme->is_loaded) {
       /* page의 할당 해제 및 page mapping 해제 */
@@ -63,6 +76,9 @@ void vm_destroy_func (struct hash_elem *e, void *aux UNUSED) {
                                          vme->vaddr)); 
       pagedir_clear_page (thread_current()->pagedir, vme->vaddr); 
     }
+
     /* vm_entry객체 할당 해제*/
     free (vme);
 }
+
+
